9026b33e25bbda0277f1a22cf7d70d5ea81924a4
idoLumigo <ido@lumigo.io>
2022-02-26 18:16:45 +0200

    add UnProtectedRouteGuard

--- libs/auth/src/lib/auth-routing.module.ts
+++ libs/auth/src/lib/auth-routing.module.ts
@@ -1,6 +1,12 @@
 import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
 import {AuthPageComponent} from "@hiboard/auth/auth-page/auth-page.component";
+import {UnProtectedRouteGuard} from "@hiboard/auth/guards/unprotected-route.guard";
+
+const guards = {
+  canLoad: [UnProtectedRouteGuard],
+  canActivate: [UnProtectedRouteGuard],
+};
 
 const authRoutes: Routes = [
   {
@@ -9,12 +15,14 @@ const authRoutes: Routes = [
     children: [
       {
         path: '',
+        pathMatch: 'full',
         redirectTo: 'login'
       },
       {
         path: 'login',
+        ...guards,
         loadChildren: () => import('@hiboard/auth-login/auth-login.module').then(({ AuthLoginModule }) => AuthLoginModule)
-      }
+      },
     ]
   }
 ]
---
+++ libs/auth/src/lib/guards/unprotected-route.guard.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, CanLoad, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import {NavigationService} from "@hiboard/navigation/navigaiton.service";
+import {AuthService} from "@hiboard/auth/state/auth.service";
+
+@Injectable({ providedIn: 'root' })
+export class UnProtectedRouteGuard implements CanLoad, CanActivate {
+  constructor(
+    private authService: AuthService,
+    private navigationService: NavigationService
+  ) {}
+
+  canActivate(): Observable<boolean | UrlTree> {
+    return this.canLoad();
+  }
+
+  canLoad(): Observable<boolean | UrlTree> {
+    return this.authService.isLoggedIn$.pipe(
+      map((isLoggedIn) => {
+        if (isLoggedIn) {
+          this.navigationService.toHome();
+        }
+
+        return true;
+      })
+    );
+  }
+}
